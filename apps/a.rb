5012d52d2f64c43a98bc836a7253ac1241e35c34
jdurkin91 <jdurkin91@gmail.com>
2018-01-19 20:23:30 -0600

    first commit

---
+++ apps/a.rb
@@ -0,0 +1,20 @@
+# a.rb
+require 'sinatra'
+
+set :count, 0
+set :port, 4567
+set :bind, '0.0.0.0'
+
+#filter to intercept request to URL
+before '/health' do
+  log_request
+end
+
+# Helper function to write
+def log_request
+  settings.count += 1
+end
+
+get '/health' do
+  "A:" + settings.count.to_s + " "
+end
---
+++ apps/b.rb
@@ -0,0 +1,20 @@
+# b.rb
+require 'sinatra'
+
+set :count, 0
+set :port, 4566
+set :bind, '0.0.0.0'
+
+#filter to intercept request to URL
+before '/health' do
+  log_request
+end
+
+# Helper function to write
+def log_request
+  settings.count += 1
+end
+
+get '/health' do
+	"B:" + settings.count.to_s + " "
+end
